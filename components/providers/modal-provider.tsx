71836e9eb52b09437f1e272f71bb826933442a48
Mohab Sherif <mohab.barca@gmail.com>
2023-09-30 16:55:56 +0300

    Added invitation feature

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -2,6 +2,7 @@
 
 import CreateServerModal from "@/components/modals/create-server-modal"
 import { useEffect, useState } from "react"
+import InviteModal from "@/components/modals/invite-modal"
 
 export const ModalProvider = () => {
     const [isMounted, setIsMounted] = useState(false)
@@ -14,6 +15,7 @@ export const ModalProvider = () => {
     return (
         <>
             <CreateServerModal />
+            <InviteModal />
         </>
     )
 }
