dbc43dec31f55cb3ee73ec1af089bbfb9871e354
Mohab Sherif <mohab.barca@gmail.com>
2023-09-30 18:51:07 +0300

    Added update server settings

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -44,7 +44,9 @@ const ServerHeader = ({
             )}
 
             {isAdmin && (
-                <DropdownMenuItem className=" px-3 py-2 text-sm cursor-pointer">
+                <DropdownMenuItem
+                    onClick={() => onOpen("editServer", { server })}
+                    className=" px-3 py-2 text-sm cursor-pointer">
                     Server Settings
                     <Settings className="h-4 w-4 ml-auto" />
                 </DropdownMenuItem>
