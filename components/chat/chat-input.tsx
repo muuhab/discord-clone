511d4267d348199bd76a7dae872b84743d334077
Mohab Sherif <mohab.barca@gmail.com>
2023-10-11 14:18:13 +0300

    Added send messages & attachment

--- components/chat/chat-input.tsx
+++ components/chat/chat-input.tsx
@@ -15,6 +15,7 @@ import { Plus, Smile } from 'lucide-react';
 import { Input } from '../ui/input';
 import qs from 'query-string';
 import axios from 'axios';
+import { useModal } from '@/hooks/use-modal-store';
 
 interface ChatInputProps {
     apiUrl: string;
@@ -33,6 +34,9 @@ const ChatInput: FC<ChatInputProps> = ({
     name,
     type
 }) => {
+
+    const { onOpen } = useModal()
+
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
         defaultValues: {
@@ -49,6 +53,7 @@ const ChatInput: FC<ChatInputProps> = ({
                 query
             })
             await axios.post(url, values)
+            form.reset()
 
         } catch (error) {
             console.log(error)
@@ -66,7 +71,7 @@ const ChatInput: FC<ChatInputProps> = ({
                             <div className="relative p-4 pb-6">
                                 <button
                                     type="button"
-                                    // onClick={() => onOpen("messageFile", { apiUrl, query })}
+                                    onClick={() => onOpen("messageFile", { apiUrl, query })}
                                     className="absolute top-7 left-8 h-[24px] w-[24px] bg-zinc-500 dark:bg-zinc-400 hover:bg-zinc-600 dark:hover:bg-zinc-300 transition rounded-full p-1 flex items-center justify-center"
                                 >
                                     <Plus className="text-white dark:text-[#313338]" />
--- pages/api/socket/io.ts
+++ pages/api/socket/io.ts
@@ -15,12 +15,14 @@ const ioHandler = (req: NextApiRequest, res: NextApiResponseServerIo) => {
     const path = "/api/socket/io";
     const httpServer: NetServer = res.socket.server as any;
     const io = new ServerIO(httpServer, {
-      path,
+      path: path,
       // @ts-ignore
       addTrailingSlash: false,
     });
     res.socket.server.io = io;
   }
+
   res.end();
 };
+
 export default ioHandler;
